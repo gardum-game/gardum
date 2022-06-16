e42b2e93a3957407a32f0098458745c9f8a8fa1b
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-06-16 22:52:35 +0300

    Update copyrights

--- src/ui/hud/health_bar.rs
+++ src/ui/hud/health_bar.rs
@@ -3,19 +3,18 @@
  *
  *  This file is part of Gardum.
  *
- *  Gardum is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
+ *  Gardum is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
  *
  *  Gardum is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *  GNU Affero General Public License for more details.
  *
- *  You should have received a get of the GNU Affero General Public License
- *  along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with Gardum. If not, see <https://www.gnu.org/licenses/>.
  */
 
 use bevy_egui::egui::*;
